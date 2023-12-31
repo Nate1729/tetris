3b4a2c4f67d893d213f032d75023b330fef40be5
Nate Ferreira <nferreira1729@gmail.com>
2023-12-31 15:31:43 -0500

    build: add check for output dir

--- configure.sh
+++ configure.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+if test -d ! out/build/; then
+	mkdir -p out/build
+fi
+
 cmake -S . -B out/build/ \
 	-DGLFW_BUILD_TESTS=OFF \
 	-DGLFW_BUILD_DOCS=OFF && cp out/build/compile_commands.json .

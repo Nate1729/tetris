d8ac087e74a705d7cd17e7c9e0844418ff4bd02a
Nate Ferreira <nferreira1729@gmail.com>
2023-12-31 16:09:56 -0500

    feat: add glad, implement some opengl code

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* This let's us include in any order.
+ */
+#define GLFW_INCLUDE_NONE
+
 #include <GLFW/glfw3.h>
+#include <glad.h>
 
 /* Error callback for GLFW. When an error occurs this
  * function will be called
@@ -18,6 +23,10 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action,
     glfwSetWindowShouldClose(window, GLFW_TRUE);
 }
 
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
 int main() {
   printf("Initializing glfw.\n");
   if (!glfwInit()) {
@@ -27,6 +36,9 @@ int main() {
 
   glfwSetErrorCallback(error_callback);
 
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
   GLFWwindow *window = glfwCreateWindow(640, 480, "Tetris", NULL, NULL);
   if (!window) {
     fprintf(stderr, "Error, couldn't create window. Exiting Early\n");
@@ -34,14 +46,25 @@ int main() {
     return EXIT_FAILURE;
   }
 
+  glfwMakeContextCurrent(window);
+
   /* Bind callback function for key presses.
    * Must be done _after_ window creation since key
    * presses are bound to a window
    */
   glfwSetKeyCallback(window, key_callback);
 
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  /* glad: Load opengl function ptrs */
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    fprintf(stderr, "Failed to initialize GLAD\n");
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
   while (!glfwWindowShouldClose(window)) {
     /* GAME LOOP */
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
 

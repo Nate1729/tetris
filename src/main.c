55143e8ca303cf63855a47a76f087c04cd352a79
Nate Ferreira <nferreira1729@gmail.com>
2024-03-02 09:49:13 -0500

    feat: do batch rendering
    Only two rectangles

--- src/main.c
+++ src/main.c
@@ -84,21 +84,64 @@ int main() {
   GLFWwindow *window = init();
   int foo = 1;
   ShaderProgram shader_program = shader_program_create_from_files(
-      "shaders/texture.vshader", "shaders/texture.fshader");
+      "shaders/simple.vshader", "shaders/orange.fshader");
 
   /*Vertex data
    */
   float vertices[] = {
-      /* positions        colors             Texture coords */
-      0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, /* Top Right*/
-      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, /* Right */
-      -0.5f, -0.5,  0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, /* Bottom Left */
-      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f  /* Top */
+      /* Left Rectangle */
+      -0.75f,
+      -0.5f,
+      0.0f,
+
+      -0.75f,
+      0.5f,
+      0.0f,
+
+      -0.25f,
+      0.5f,
+      0.0f,
+
+      -0.25f,
+      -0.5f,
+      0.0f,
+
+      /* Right Rectangle */
+      0.75f,
+      -0.5f,
+      0.0f,
+
+      0.75f,
+      0.5f,
+      0.0f,
+
+      0.25f,
+      0.5f,
+      0.0f,
+
+      0.25f,
+      -0.5f,
+      0.0f,
   };
-  unsigned int indices[] = {// First Triangle
-                            0, 1, 3,
-                            // Second Triangle
-                            1, 2, 3};
+  unsigned int indices[] = {
+      // First Triangle
+      0,
+      1,
+      2,
+      // Second Triangle
+      0,
+      3,
+      2,
+      // Third Triangle
+      4,
+      5,
+      6,
+      // Fourth Triangle
+      4,
+      7,
+      6,
+
+  };
   unsigned int VBO, VAO, EBO;
   // Bind Vertex Array Object (VAO)
   glGenVertexArrays(1, &VAO);
@@ -108,9 +151,6 @@ int main() {
   // Bind VAO
   glBindVertexArray(VAO);
 
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
                GL_STATIC_DRAW);
@@ -119,45 +159,8 @@ int main() {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
   // Position attribute
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
   glEnableVertexAttribArray(0);
-  // color attribute
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
-  // Texture coord attribute
-  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
-                        (void *)(6 * sizeof(float)));
-  glEnableVertexAttribArray(2);
-
-  // Other stuff -- Not sure what this does
-  // glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-  // glBindVertexArray(0);
-
-  unsigned int texture;
-  glGenTextures(1, &texture);
-  glBindTexture(GL_TEXTURE_2D, texture);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  int width, height, nrChannels;
-  unsigned char *data =
-      stbi_load("container.jpg", &width, &height, &nrChannels, 0);
-
-  if (data) {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-                 GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-  } else {
-    fprintf(stderr, "Failed to load texture!\n");
-  }
-  stbi_image_free(data);
-
-  // Moved use program outside of game loop
-  glUseProgram(shader_program);
-  shader_program_set_uniform_i(shader_program, "ourTexture", 0);
 
   while (!glfwWindowShouldClose(window)) {
     /* GAME LOOP */
@@ -165,10 +168,9 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBindTexture(GL_TEXTURE_2D, texture);
     glUseProgram(shader_program);
     glBindVertexArray(VAO);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -176,7 +178,7 @@ int main() {
 
   glDeleteVertexArrays(1, &VAO);
   glDeleteBuffers(1, &VBO);
-  glDeleteBuffers(1, &EBO);
+  // glDeleteBuffers(1, &EBO);
   glDeleteProgram(shader_program);
   glfwDestroyWindow(window);
   glfwTerminate();
